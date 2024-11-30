ac00ec28c50b6930133215a936dca441df9694a6
Sharik <sharik.abubucker@skyconseil.fr>
2024-11-30 14:13:39 +0100

    add new title

--- sharik.Portefolio/src/App.jsx
+++ sharik.Portefolio/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Sharik portefolio</h1>
+      <h1>Sharik portefolio and blog</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
